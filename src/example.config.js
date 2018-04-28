9b09914881fd4aa2f98574690e13b53f94f2e4ca
Huy Tran <kingbazoka@gmail.com>
2018-04-27 20:29:59 -0700

    Use external API to storage data

---
+++ src/example.config.js
@@ -0,0 +1,5 @@
+// Get these info from jsonbin.io
+module.exports = {
+    SECRET: 'YOUR_SECRET_KEY',
+    URL: 'YOUR_API_URL'
+};
--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 require('./index.html');
 require('./main.scss');
 
+const config = require('./config');
 const Elm = require('./Main.elm');
-const StorageKey = 'elm-data';
 
-let storedData = localStorage.getItem(StorageKey);
-let state = storedData ? JSON.parse(storedData) : null;
-var app = Elm.Main.fullscreen(state);
+fetch(config.URL, {
+    headers: {
+        "secret-key": config.SECRET
+    }
+})
+.then(res => res.json())
+.then(json => {
+    var app = Elm.Main.fullscreen(json);
 
-app.ports.setStorage.subscribe(function(state) {
-  localStorage.setItem(StorageKey, JSON.stringify(state));
+    app.ports.setStorage.subscribe(function(state) {
+        fetch(config.URL, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+                "secret-key": config.SECRET
+            },
+            data: JSON.stringify(state)
+        });
+    });
 });
