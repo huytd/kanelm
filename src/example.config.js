40483907122e141d859df4785ae4ad73cbff61b9
evanfrawley <frawley@uw.edu>
2018-04-30 11:35:55 -0700

    Update example config with readme example

--- src/example.config.js
+++ src/example.config.js
@@ -1,5 +1,5 @@
 // Get these info from jsonbin.io
 module.exports = {
-    SECRET: 'YOUR_SECRET_KEY',
-    URL: 'https:{YOUR_API_URL}'
+    SECRET: 'you-api-secret-here',
+    URL: 'https://api.jsonbin.io/b/{id-of-jsonbin-bin}'
 };
